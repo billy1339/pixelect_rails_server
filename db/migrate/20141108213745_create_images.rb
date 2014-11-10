355f923f2a695fedbbe49556004710d796d1a926
IljaGoushcha <iljagou@yahoo.com>
2014-11-10 11:48:28 -0500

    added tags route, added image_file to images migration

--- db/migrate/20141108213745_create_images.rb
+++ db/migrate/20141108213745_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.string :file_name
+      t.string :image_file
       t.string :image_url
       t.integer :flag
       t.belongs_to :image_set, index: true
